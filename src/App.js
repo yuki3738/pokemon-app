273f82c36a77020de35a48639739bc07770366f7
yuki3738 <y.minamiya@gmail.com>
2023-05-06 00:22:08 +0900

    10まで https://www.udemy.com/course/react-3project-app-udemy/learn/lecture/32873982#overview

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useEffect } from "react";
 import './App.css';
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
+
 
   return (
     <div className="App"></div>
