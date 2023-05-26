7cfe6a73f078a4580cb85d19b2042f311015f8eb
yuki3738 <y.minamiya@gmail.com>
2023-05-26 23:42:27 +0900

    15 まで https://www.udemy.com/course/react-3project-app-udemy/learn/lecture/32893136#overview

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ pokemon }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="card">
+      <div>
+        <img src={pokemon.sprites.front_default} alt=""  />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <div className="cardInfo">
+          <div className="cardData">
+            <p className="title">重さ：{pokemon.weight}</p>
+          </div>
+          <div className="cardData">
+            <p className="title">高さ：{pokemon.hight}</p>
+          </div>
+          <div className="cardData">
+            <p className="title">アビリティ：{pokemon.abilities[0].ability.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card
